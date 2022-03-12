42a5c66a431cbe9af8b7ddd87cfb90bc87ac1e71
abbabashir <abbabashir516@gmail.com>
2022-03-12 11:28:53 +0100

    added

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -13,117 +13,166 @@ class _LoginPageState extends State<LoginPage> {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: w,
-            height: h * 0.3,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  "assets/loginimg.png"
-              ),
-              fit: BoxFit.cover
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: w,
+              height: h * 0.2,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/loginimg.png"
+                ),
+                fit: BoxFit.cover
+                ),
               ),
             ),
-          ),
-         
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            width: w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Grand",
-                  style: TextStyle(
-                    fontSize: 70,
-                    fontWeight: FontWeight.bold
+           
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Grand",
+                    style: TextStyle(
+                      fontSize: 70,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
-                ),
-                Text(
-                  "Entertainment",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                  Text(
+                    "Entertainment",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
-                ),
-                SizedBox(height: 50,),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        spreadRadius: 5,
-                        offset: Offset(1, 1),
-                        color: Colors.black.withOpacity(0.3)
-                      )
-                    ]
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 1.0,
+                  SizedBox(height: 50,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                          offset: Offset(1, 1),
+                          color: Colors.black.withOpacity(0.3)
+                        )
+                      ]
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 1.0,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
                         ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        spreadRadius: 5,
-                        offset: Offset(1, 1),
-                        color: Colors.black.withOpacity(0.3)
-                      )
-                    ]
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 1.0,
+                  SizedBox(height: 20,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                          offset: Offset(1, 1),
+                          color: Colors.black.withOpacity(0.3)
+                        )
+                      ]
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 1.0,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
                         ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(),
+                        ),
+                        Text(
+                    "forgot your password?",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[500]
+                    ),
+                  ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: w*0.5,
+              height: h *0.08,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/loginbtn.png"
+                ),
+                fit: BoxFit.cover
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            RichText(text: TextSpan(
+              text: "Don\'t have an account?"
+            ),
+            ),
+          ],
+        ),
       ),
     );
   }
