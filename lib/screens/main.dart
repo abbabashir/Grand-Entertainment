974d4743a49ca795a44bcc07539f76adc8012abc
abbabashir <abbabashir516@gmail.com>
2022-03-22 23:55:17 +0100

    firebase auth

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,14 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:grand_entertainment/screens/forgotpassword.dart';
-import 'package:grand_entertainment/screens/homepage.dart';
-import 'package:grand_entertainment/screens/loginPage.dart';
-import 'package:grand_entertainment/screens/payment.dart';
-import 'package:grand_entertainment/screens/signup.dart';
-import 'package:grand_entertainment/screens/welcomePage.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:grand_entertainment/auth/authentication.dart';
+import 'package:grand_entertainment/screens/loginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:grand_entertainment/firebase_options.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+
+
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,13 +21,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        Provider<Authentication>(
+          create: (_) => Authentication(FirebaseAuth.instance)
+          ),
+          
+      ],
+      child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Grand Entertainment',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PaymentPage(),
+      home: AuthenticationWrapper(),
+    ),
+      );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
     );
   }
 }
